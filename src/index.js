f5020b60896e2a884f89999d6b23b82ffdc276f1
Valerii-Postrybailo <postribailo.valeri@gmail.com>
2022-09-21 04:41:23 +0300

    table murkup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const taskCardEl = document.querySelector(".notes-table__to-do-card");
+const generalTasksInfo = document.querySelector(".general-info-table__to-do-card")
+
+const btnRefs = {
+  createTaskBtn:document.querySelector(".create-btn"),
+  rewriteTaskBtn:document.querySelector(".rewrite-task-btn"),
+  archiveTaskBtn: document.querySelector(".archive-task-btn"),
+  deleteTaskBtn: document.querySelector(".delete-task-btn")
+}
