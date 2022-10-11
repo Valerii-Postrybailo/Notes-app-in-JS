0118d844b85720ff594a19a7106203748167bd8b
Valerii-Postrybailo <99152023+Valerii-Postrybailo@users.noreply.github.com>
2022-10-11 12:21:27 +0000

    Deploying to gh-pages from @ Valerii-Postrybailo/Notes-app-in-JS@ccf22552fbf0f69dd601c1f90d9d1f39eee56954 🚀

---
+++ index.2a8f7047.js
@@ -0,0 +1,2 @@
+document.querySelector(".notes-table__to-do-card"),document.querySelector(".general-info-table__to-do-card"),document.querySelector(".create-btn"),document.querySelector(".rewrite-task-btn"),document.querySelector(".archive-task-btn"),document.querySelector(".delete-task-btn");
+//# sourceMappingURL=index.2a8f7047.js.map
